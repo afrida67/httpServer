df23ceeb56b17992813a20a9d8901d5d8e8f34a3
Afrida Aesha <afrida660@gmail.com>
2019-02-13 03:15:10 +0600

    Request to server done

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const http = require('http');
+const port = 3000;
+const server = http.createServer();
+const { parse } = require('querystring');
 
-//creating server object
-http.createServer ((request, response) => {
-    response.write('Check Response');
-    response.end();
-}).listen(3000, () => {
-    console.log('Server start at port 3000');
+server.on("request", (req, res) => {
+    if (req.method === 'POST'){
+        let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        console.log(
+            parse(body)
+        );
+        res.end('Saved to Database');
+    });
+   }
+   else{
+       res.end(`
+       <!doctype html>
+       <html>
+       <body>
+           <form action="/" method="post">
+             Name <input type="text" name="name" /><br />
+             Roll <input type="number" name="age" /><br />
+             Department <input type="text" name="dept" /><br />
+               <button>Save</button>
+           </form>
+       </body>
+       </html>
+   `);
+   }
 });
+
+// Start server
+server.listen(port, () => console.log(`Server listening on port ${port}`));
