f4e8be72d6b362ad5e3b8034475c93e1b932bd27
Afrida Anzum <afridaaesha.dsi@gmail.com>
2019-02-14 13:29:25 +0600

    static post

--- app.js
+++ app.js
@@ -2,41 +2,40 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer();
 const { parse } = require('querystring');
+const mysql = require('mysql');
 
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "sys"
+    });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 server.on('request', (req, res) => {
     if (req.method === 'POST'){
         let body = '';
         req.on('data', chunk => {
             body += chunk.toString();
-            body = JSON.stringify(body);
-   
+           // body = JSON.stringify(body);
+
         });
+
         req.on('end', () => {
-            console.log(
-                parse(body)
-            );
-            const mysql = require('mysql');
+            console.log(parse(body));
 
-                let con = mysql.createConnection({
-                host: "localhost",
-                user: "root",
-                password: "123456",
-                database: "sys"
-                });
-
-                con.connect(function(err) {
-                    if (err) throw err;
-                    console.log("Connected!");
-
-                let sql = "INSERT INTO students ('"+body.name+"') VALUES (?)";
-                
-                con.query(sql, function (err, result) {
+              //  let sql = "INSERT INTO students ('"+body.name+"') VALUES (?)";
+              let sql = "INSERT INTO students (name, grade) VALUES ('Afrida',67)";     
+               con.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log("1 record inserted");
                 });
                 })
             res.end('Saved to Database');
-        });
+        
    }
    else{
        res.end(`
