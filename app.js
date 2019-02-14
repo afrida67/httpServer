5480f81986e5a3e2839d1a80725973c673c27257
Afrida Aesha <afrida660@gmail.com>
2019-02-15 00:32:15 +0600

    update request handled

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+'use strict';
+
 const http = require('http');
-const port = 3000;
-const server = http.createServer();
 const { parse } = require('querystring');
 const mysql = require('mysql');
 const url = require('url'); 
 
+const port = 3000;
+
+const server = http.createServer();
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+    });
+
+/*
 let con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "123456",
     database: "sys"
-    });
+    });*/
 
 con.connect(function(err) {
     if (err) throw err;
@@ -20,7 +32,8 @@ con.connect(function(err) {
 server.on('request', (req, res) => {
 
     const reqUrl = url.parse(req.url, true);
-
+    
+    //*** POST ***/
     if (req.method === 'POST'){
         let body = '';
         req.on('data', chunk => {
@@ -32,7 +45,7 @@ server.on('request', (req, res) => {
             let data = parse(body);              
             let sql = `INSERT INTO students (name, grade) VALUES ('${data.name}',${data.grade})`;    
 
-               con.query(sql, function (err, result) {
+            con.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log(result);
                 });
@@ -40,45 +53,48 @@ server.on('request', (req, res) => {
             res.end('Saved to Database');
         
    }
-
+   //*** GET ***/
    else if (reqUrl.pathname === '/get'){
 
         req.on('end', () => {   
-            let id= reqUrl.query.id;                      
-            let sql = `Select * from students where id= ${id}`;     
+            let id = reqUrl.query.id;                      
+            let sql = `Select * from students where id= ${id}`; 
+
             con.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log(result);
                 });
                 })
-            res.end();   
+            res.end('Showing Student Information');   
     }
+     //*** DELETE ***/
     else if (reqUrl.pathname === '/delete'){
 
             req.on('end', () => { 
-                let id= reqUrl.query.id;                        
-                let sql = `Delete from students where id= ${id}`;     
+                let id = reqUrl.query.id;                        
+                let sql = `Delete from students where id= ${id}`;  
+
                 con.query(sql, function (err, result) {
                     if (err) throw err;
                     console.log(result);
                     });
                     })
-                res.end();   
+                res.end('Deletion Successful');   
     }
+     //*** UPDATE ***/
     else if (reqUrl.pathname === '/update'){
 
         req.on('end', () => { 
-           // let sql = "UPDATE students SET name = 'Moyna' WHERE grade = 56";
-           let name= reqUrl.query.name;
-          // let grade= reqUrl.query.grade;
+           let name = reqUrl.query.name;
+           let id = reqUrl.query.id;  
+           let sql = `UPDATE students SET name= '${name}' WHERE id= ${id}`;   
 
-           let sql = `UPDATE students SET name= ${name} WHERE id= 14`;    
             con.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log(result);
                 });
                 })
-            res.end();   
+            res.end('DB has been updated');   
     }
    else{
        res.end(`
@@ -87,7 +103,7 @@ server.on('request', (req, res) => {
        <body>
            <form action="/" method="post">
              Name <input type="text" name="name" /><br />
-             Roll <input type="number" name="grade" /><br />
+             Grade <input type="number" name="grade" /><br />
                <button>Save</button>
            </form>
 
