d855f7bf772f8f7baa7b3f1f623ffe57803bf4e2
Afrida Aesha <afrida660@gmail.com>
2019-02-25 00:48:05 +0600

    POST GET worked, delete not working

--- app.js
+++ app.js
@@ -1,205 +1,88 @@
 'use strict';
-
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const mysql = require('mysql');
-const url = require('url');
-const fs = require('fs');
 
 const port = 3000;
 
-const server = http.createServer();
-
-let config = {
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "sys"
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'sys',
 };
 
-server.on('request', (req, res) => {
+const pool = mysql.createPool(config);
 
-    const reqUrl = url.parse(req.url, true);
+//  body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
-    let path = reqUrl.pathname;
-    let name = reqUrl.query.name;
-    let id = reqUrl.query.id;
-    let grade = reqUrl.query.grade;
-    let sql;
-    let connection;
-
-    switch (path) {
-
-        /*** POST ***/
-        case '/post':
-
-            connection = mysql.createConnection(config);
-            connection.connect(function(err) {
-                if (err) {
-                    console.log(err);
-                    res.writeHead(500);
-                    res.write(`Server failed to connect`);
-                    res.end();
-                } else {
-                    sql = `INSERT INTO students (name, grade) VALUES ('${name}',${grade})`;
-                    connection.query(sql, function(err, result) {
-                        if (err) {
-                            console.log(err);
-                            res.writeHead(400);
-                            res.write(`Bad request. Fill up all`);
-                            res.end();
-                        } else {
-                            console.log(result);
-                            console.log(`New Id Created: ${name} ${grade}`);
-                            res.writeHead(200);
-                            res.write(`New Student name : ${name} and grade : ${grade}`);
-                            res.end();
-                        }
-                    });
-                }
-                connection.end();
-            });
-            break;
-
-            /*** GET ***/
-        case '/get':
-
-            connection = mysql.createConnection(config);
-            connection.connect(function(err) {
-                if (err) {
-                    console.log(err);
-                    res.writeHead(500);
-                    res.write(`Server failed to connect`);
-                    res.end();
-                } else {
-                    sql = `Select * from students WHERE id= ${id}`;
-                    connection.query(sql, function(err, result) {
-                        if (err) {
-                            console.log(err);
-                            res.writeHead(400);
-                            res.write(`Bad request`);
-                            res.end();
-                        } else {
-                            if (!result.length) {
-                                res.writeHead(404);
-                                res.write(`Id doesn't exist, enter valid id`);
-                                res.end();
-                                console.log(`404 not found`);
-
-                            } else {
-                                let string = JSON.stringify(result);
-                                let json = JSON.parse(string);
-                                res.writeHead(200);
-                                res.write(`Showing the student information : name: ${json[0].name}, grade : ${json[0].grade}`);
-                                res.end();
-                                console.log(`name: ${json[0].name}, grade : ${json[0].grade}`);
-                            }
-                        }
-                    });
-                }
-                connection.end();
-            });
-            break;
-
-            /*** DELETE ***/
-
-        case '/delete':
-
-            connection = mysql.createConnection(config);
-            connection.connect(function(err) {
-                if (err) {
-                    console.log(err);
-                    res.writeHead(500);
-                    res.write(`Server failed to connect`);
-                    res.end();
-                } else {
-                    sql = `Delete from students  WHERE id= ${id}`;
-                    connection.query(sql, function(err, result) {
-                        if (err) {
-                            console.log(err);
-                            res.writeHead(400);
-                            res.write('Bad Request');
-                            res.end();
-                        } else {
-
-                            if (result.affectedRows == 0) {
-                                res.writeHead(404);
-                                res.write(`Id doesn't exist, enter valid id`);
-                                res.end();
-                                console.log(`404 not found`);
-
-                            } else {
-                                res.writeHead(200);
-                                res.write(`Deletion Successful`);
-                                res.end();
-                                console.log(`Deleted`);
-                            }
-                        }
-                    });
-                }
-                connection.end();
-            });
-
-            break;
-
-            /*** UPDATE ***/
-
-        case '/update':
-
-            connection = mysql.createConnection(config);
-            connection.connect(function(err) {
-                if (err) {
-                    console.log(err);
-                    res.writeHead(500);
-                    res.write(`Server failed to connect`);
-                    res.end();
-                } else {
-                    sql = `UPDATE students SET name= '${name}' WHERE id= ${id}`;
-
-                    connection.query(sql, function(err, result) {
-                        if (err) {
-                            console.log(err);
-                            res.writeHead(400);
-                            res.write(`Bad Request`);
-                            res.end();
-
-                        } else {
-                            if (result.affectedRows == 0) {
-                                res.writeHead(404);
-                                res.write(`Id doesn't exist, enter valid id`);
-                                res.end();
-                                console.log(`404 not found`);
-
-                            } else {
-                                console.log(result);
-                                res.writeHead(200);
-                                res.write(`Updated name: ${name}`);
-                                res.end();
-                            }
-                        }
-                    });
-                }
-                connection.end();
-            });
-            break;
-
-        default:
-
-            fs.readFile('index.html', function(err, data) {
-                if (err) {
-                    res.writeHead(404);
-                    res.write(`File not found`);
-                    res.end();
-                } else {
-                    res.writeHead(200, {
-                        'Content-Type': 'text/html', 
-                    });
-                    res.write(data.toString());
-                    res.end();
-                }
-            });
-    }
-
+app.get('/', (request, response) => {
+    response.send({
+        message: 'Create New Student Account'
+    });
 });
 
-// Start server
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+// add a new student
+app.post('/api/post', (request, response) => {
+    pool.query(`INSERT INTO students SET ?`, request.body, (err, result) => {
+        if (err) throw err;
+
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
+
+// display a single student
+app.get('/api/get/:id?', (request, response) => {
+    const paramId = request.params.id;
+    const queryId = request.query.id;
+    let id;
+
+    if(paramId && !queryId)
+        id = paramId;
+    else if(queryId && !paramId)
+        id = queryId;
+
+    if(!id)
+        return response.send(`Please specify an id`);
+
+    pool.query(`SELECT * FROM students WHERE id = ${id}`, id, (err, result) => {
+        if (err) throw err;
+
+        response.send(result);
+    });
+});
+
+
+// delete a student id
+app.delete('/api/del/:id?', (request, response) => {
+    const paramId = request.params.id;
+    const queryId = request.query.id;
+    let id;
+
+    if(paramId && !queryId)
+        id = paramId;
+    else if(queryId && !paramId)
+        id = queryId;
+
+    if(!id)
+        return response.send(`Please specify an id`);
+
+    pool.query(`DELETE FROM students WHERE id = ${id}`, id, (err, result) => {
+        if (err) throw err;
+        response.send('ID deleted.');
+    });
+});
+
+
+
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
